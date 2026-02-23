7e9e85c2ad328c2b92960e308642d239f5172dc0
LKayZe <loriscaze@gmail.com>
2026-02-23 13:42:09 +0100

    fix import

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import requests
 from bs4 import BeautifulSoup
 import itertools
 import pandas as pd
-from streamlit_sortables import sort_items
 from betclic_early_win_scraper import scrape_betclic_early_win
 from winamax_scraper import scrape_winamax_football
 from psel_scraper import scrape_psel_football
